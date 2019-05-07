33642f83370fe2a30f20d195f179346e9922ded2
Yanis-Laptop <yanis.attia@outlook.fr>
2019-05-07 17:13:05 +0200

    J'SUIS UNE FORMULE UNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN

--- proj_regulateur/test/gps/TestRegulateur.java
+++ proj_regulateur/test/gps/TestRegulateur.java
@@ -4,12 +4,18 @@ import gps.implementations.GPSPosition;
 import gps.interfaces.Position;
 import regulateur.implementations.Regulator;
 
+import java.util.Scanner;
+
 public class TestRegulateur {
     public static void main(String[] args){
         Position position = new GPSPosition();
         Regulator regulateur = new Regulator();
 
-        regulateur.start(50);
+        System.out.println("Vitesse désirée ? ");
+        Scanner keyboard = new Scanner(System.in);
+        float vit = keyboard.nextFloat();
+
+        regulateur.start(vit);
         regulateur.calcTargetSpeed(position);
     }
 }
