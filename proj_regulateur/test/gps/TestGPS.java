91b10158c78c76531fb4d1acc4ee4fa00ae0dab1
Yanis-Laptop <yanis.attia@outlook.fr>
2019-05-07 15:10:50 +0200

    Slt

--- proj_regulateur/test/gps/TestGPS.java
+++ proj_regulateur/test/gps/TestGPS.java
@@ -10,8 +10,17 @@ public class TestGPS {
         Coordinate test = new Latitude();
 
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Entrez la valeur en radians et pas en degré!");
+        System.out.println("Entrez la valeur en radians (latitude) et pas en degré!");
         int rad = keyboard.nextInt();
+
+        test.setValue(rad);
+
+        System.out.println(test.getDeg());
+        System.out.println(test.toString());
+
+        test = new Longitude();
+        System.out.println("Entrez la valeur en radians (longitude) et pas en degré!");
+        rad = keyboard.nextInt();
 
         test.setValue(rad);
 
