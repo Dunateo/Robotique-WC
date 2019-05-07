46e2f57644ee433d6aaefcac7ad5008ee6baf19f
Yanis-Laptop <yanis.attia@outlook.fr>
2019-05-07 16:08:12 +0200

    Calcul des positions et du regulateur.

--- proj_regulateur/src/regulateur/implementations/Regulator.java
+++ proj_regulateur/src/regulateur/implementations/Regulator.java
@@ -1,8 +1,37 @@
 package regulateur.implementations;
 
+import gps.interfaces.Position;
 import regulateur.interfaces.Manager;
 import regulateur.interfaces.Calculator;
 
-public class Regulator{
+public class Regulator implements Manager, Calculator{
+    private boolean working;
+    private float targetSpeed;
 
+    Regulator(){
+        working = false;
+    }
+
+    public boolean isWorking(){
+        return working;
+    }
+
+    public int start(float speed){
+        working = true;
+        targetSpeed = speed;
+        return 0;
+    }
+
+    public int stop(){
+        working = true;
+
+        return 0;
+    }
+
+    public float calcTargetSpeed(Position position){
+        float speed = targetSpeed;
+
+        
+        return speed;
+    }
 }
