f4e5ed867803df7da6927973c63213f4e0b41908
Valentin Bru <bruvalentin84@icloud.com>
2019-05-09 09:44:58 +0200

    Ajout de commentaires pour Cartographie Java

--- proj_regulateur/src/carto/implementations/Cartographie.java
+++ proj_regulateur/src/carto/implementations/Cartographie.java
@@ -4,6 +4,12 @@ import carto.interfaces.Cartograph;
 import gps.interfaces.Position;
 
 public class Cartographie implements Cartograph {
+
+    /**
+     * renvoie la limite de vitesse 
+     * @param position
+     * @return
+     */
     @Override
     public float getSpeedLimit(Position position) {
         return 80;
